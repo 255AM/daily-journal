63a5af9da95e8d4c65ce9591ab5cfad2d04be81d
Chance Lane <lanecw@gmail.com>
2021-01-19 09:09:38 -0500

    working on daily journal

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
 console.log("Welcome to the main module")
+
+let diary = [];
+let newEntry = {}
+
+function addEntry(){
+    console.log('working')
+    newEntry = {
+        date:document.getElementById('date').value,
+        concepts:document.getElementById('concepts').value,
+        jentry: document.getElementById('jentry').value
+    }
+    console.log(newEntry)
+    diary.push(newEntry)
+    
+}
+
+
+addEntry()
+var inputs = document.getElementById("my-form").elements;
+console.log(inputs)
